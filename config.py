69733a702fda7366e350907c46590669c7d30709
Sabir Shaikh <44839291+therealsabirs@users.noreply.github.com>
2025-03-24 12:22:57 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+class Config:
+    SECRET_KEY = "your_secret_key"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:sabir@localhost/assetmanagment"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
